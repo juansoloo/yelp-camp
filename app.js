c4b3bcc6b20953e221218e3ab640a33677efff12
Juan Hurtado <hurtadojuan93@yahoo.com>
2024-06-20 21:28:39 -0400

    added method override and created the edit page for existing campgorunds

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelpcamp')
+mongoose.connect('mongodb://localhost:27017/yelpcamp');
 
 const db = mongoose.connection; 
 db.on("error", console.error.bind(console, "connection, error"));
@@ -16,6 +16,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true }));
+
 app.get('/',(req,res) => {
     res.render('home')
 })
@@ -26,11 +28,29 @@ app.get('/campgrounds',async (req,res) => {
     res.render('campgrounds/index', { campgrounds });
 })
 
+app.get('/campgrounds/new', (req,res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req,res) => {
     const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', { campground })
+    res.render('campgrounds/show', { campground });
 })
+
+app.get('/campgrounds/:id/:edit', async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+});
+
+
+
 
 app.listen(3000, () => {
     console.log('serving on port 3000')
-})
+});
