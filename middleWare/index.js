2d6652c9bb17d330ba85b611a010b44a0dfff8d2
Juan Hurtado <hurtadojuan93@yahoo.com>
2024-06-25 00:22:31 -0400

    created logic for user authenication

--- middleWare/index.js
+++ middleWare/index.js
@@ -4,10 +4,22 @@ const morgan = require('morgan');
 
 app.use(morgan('tiny'));
 app.use((req,res,next) => {
-    req.requestTime = Date.now();
-    req.method = 'GET';
+    req. requestTime = Date.now();
     console.log(req.method, req.path);
     next();
+})
+
+app.use('/dogs', (req,res,next) => {
+    console.log('I love dogs')
+    next();
+})
+
+app.use((req,res,next) => {
+    const { password } = req.query;
+    if (password === 'chickennugget') {
+        next()
+    }
+    res.send('SORRY YOU NEED A PASSORD')
 })
 
 
@@ -28,14 +40,23 @@ app.use((req,res,next) => {
 // })
 
 app.get('/', (req,res) => {
-    console.log(`REQUEST DATE: ${req.requestTime}`)
+    console.log(`REQUEST TIME: ${req.requestTime}`)
     res.send('HOME PAGE')
 })
+
+
 app.get('/dogs', (req,res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('woof woof')
 })
 
+app.get('/secret', (req,res) => {
+    res.send('MY SECRET IS: Sometimes i wear headphones in public so i dont have to speak to anyone')
+})
+
+app.use((req,res) => {
+    res.status(404).send('NOT FOUND!')
+})
 
 app.listen(3000, () => {
     console.log('App is running on localhost:3000')
