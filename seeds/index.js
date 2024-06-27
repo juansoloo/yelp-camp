78c6b6a896a403da536704efa37886e09e584a52
Juan Hurtado <hurtadojuan93@yahoo.com>
2024-06-26 23:14:15 -0400

    created the logic for the api call from upsplash

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities'); 
 const { places,descriptors} = require('./seedsHelpers')
 const Campground = require('../models/campground');
+const axios = require('axios');
 
 mongoose.connect('mongodb://localhost:27017/yelpcamp')
 
@@ -13,18 +14,45 @@ db.once("open", () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000)
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
+  async function seedImg() {
+    try {
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: 'fN3HfX4588uVJW9FFUswdYKGPpW5v9gmdBFooFKa6jk',
+          collections: 'p2bLSJgDPjk',
+          count: 30 
+        },
+        headers: { Accept: "application/json", "Accept-Encoding": "identity" }
+      })
+      return resp.data.map((a) => a.urls.small)
+    } catch (err) {
+      console.error(err)
     }
-}
+  }
+
+  const seedDB = async () => {
+    await Campground.deleteMany({})
+    for (let i = 0; i < 20; i++) {
+      // setup
+      const placeSeed = Math.floor(Math.random() * places.length)
+      const descriptorsSeed = Math.floor(Math.random() * descriptors.length)
+      const citySeed = Math.floor(Math.random() * cities.length)
+      const price = Math.floor(Math.random() * 25) + 10;
+      const imgs = await seedImg();
+   
+      // seed data into campground
+      const camp = new Campground({
+        image: sample(imgs),
+        title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
+        location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+        description:
+          'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
+        price,
+      })
+   
+      await camp.save()
+    }
+  }
 
 seedDB().then(() => {
     mongoose.connection.close()
