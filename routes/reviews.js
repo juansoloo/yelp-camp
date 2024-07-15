80a31f8d47548f5b64bab2784bce6b5406131f3d
Juan Hurtado <hurtadojuan93@yahoo.com>
2024-07-14 21:38:33 -0400

    abstracted away the routes for review and campground creation

---
+++ routes/reviews.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const catchAsync = require('../utils/catchAsync');
+const { reviewSchema } = require('../schema.js')
+const Campground = require('../model/campground');
+const Review = require('../model/review');
+const expressError = require('../utils/expressError');
+
+
+const ValidateReview = (req,res,next) => {
+    const { error } = reviewSchema.validate(req.body);
+    console.log(error);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new expressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+router.post('/', ValidateReview, catchAsync(async(req,res) => {
+    const campground = await Campground.findById(req.params._id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+router.delete('/:_reviewId',catchAsync(async (req,res) => {
+    const { _id, _reviewId } = req.params;
+    await Campground.findByIdAndUpdate(_id, { $pull: {reviews: _reviewId} });
+    await Review.findByIdAndDelete(_reviewId);
+    res.redirect(`/campgrounds/${_id}`);
+}))
+
+module.exports = router;
