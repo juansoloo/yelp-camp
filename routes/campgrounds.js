7f7db71c9a1eaf34df39ea5ac5b4ead1bc8edff9
Juan Hurtado <hurtadojuan93@yahoo.com>
2024-08-04 21:25:11 -0400

    add logic to upload files from new.ejs

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,12 +5,18 @@ const { campgroundSchema } = require('../schema.js');
 const Campground = require('../model/campground');
 const expressError = require('../utils/expressError');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const { isLoggedIn,isAuthor,validateCampground,storeReturnTo } = require('../middleware');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req,res) => {
+        console.log([req.body, req.files]);
+        res.send('files uploaded')
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
