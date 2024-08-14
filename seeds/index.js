980df523df533c5ec685eab93c18ceeb3f220063
Juan Hurtado <hurtadojuan93@yahoo.com>
2024-08-13 22:20:43 -0400

    added missing packages

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: '66a85b2039c40882bf3a09cf',
+      author: '66bc0fd8cdd5f4b9d736691b',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
