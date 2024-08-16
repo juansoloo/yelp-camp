4237ffd3248bde1f44ddeb8ab23a5d385968e6b0
Juan Hurtado <hurtadojuan93@yahoo.com>
2024-08-15 20:54:48 -0400

    Added helmet securities policies

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const session = require('express-session');
 const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./model/user');
+const helmet = require('helmet');
 
 const userRoutes = require('./routes/user')
 const campgroundRoutes = require('./routes/campgrounds');
@@ -55,9 +56,48 @@ const secretConfig = {
     }
 };
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/"
+];
+const connectSrcUrls = [
+    "https://api.maptiler.com/"
+];
+const fontSrcUrls = [];
+
 app.use(session(secretConfig));
 app.use(flash());
-
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc:[],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dhknjxmpc/",
+                "https://api.maptiler.com/"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls]
+        }
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
